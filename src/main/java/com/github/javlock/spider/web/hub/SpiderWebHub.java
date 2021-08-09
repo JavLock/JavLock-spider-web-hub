ef8f21dbe897f67de427e9725c3e566becce813a
JavLock <jlocks1641@gmail.com>
2021-08-09 13:49:19 +0000

    start

---
+++ src/main/java/com/github/javlock/spider/web/hub/SpiderWebHub.java
@@ -0,0 +1,19 @@
+package com.github.javlock.spider.web.hub;
+
+public class SpiderWebHub extends Thread {
+
+	public static void main(String[] args) {
+		SpiderWebHub spiderWebHub = new SpiderWebHub();
+		spiderWebHub.init();
+		spiderWebHub.start();
+	}
+
+	private void init() {
+
+	}
+
+	@Override
+	public void run() {
+
+	}
+}
